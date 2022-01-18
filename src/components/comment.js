159f0239fdae650f620fd4f66693a0a8bfe11c6e
Henrique Paredes <henriqueparedes@hotmail.com>
2022-01-18 20:33:21 -0300

    initial commit

---
+++ src/components/comment.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../css/comment.css'
+
+const Comment = () => (
+    <div className="Comment">
+        <div>João:</div>
+        <div>Olá, tudo bem?</div>
+    </div>
+);
+
+export default Comment;
