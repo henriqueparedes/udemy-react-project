21b6deab5e722118efc93b82138490fa96356231
Henrique Paredes <henriqueparedes@hotmail.com>
2022-01-18 20:53:33 -0300

    Working with props

--- src/App.js
+++ src/App.js
@@ -6,9 +6,13 @@ function App() {
   return (
     <div className="App">
       <h1>Meu projeto</h1>
-      <Comment/>
-      <Comment/>
-      <Comment/>
+      <Comment name="João" email="joao@mail.com" date={new Date(2020, 3, 19)}>
+        Olá, tudo bem?
+      </Comment>
+
+      <Comment name="Maria" email="maria@mail.com" date={new Date(2020, 3, 21)}>
+        Olá, tudo bem sim...
+      </Comment>
     </div>
   );
 }
