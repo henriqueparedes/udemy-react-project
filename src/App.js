3f2c24cab333e1e91a200c73fdb511526c70888d
Henrique Paredes <henriqueparedes@hotmail.com>
2022-01-18 21:18:07 -0300

    using state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
+import React, { Component } from 'react'
 import './css/App.css';
 
 import Comment from './components/comment.js'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Meu projeto</h1>
-      <Comment name="João" email="joao@mail.com" date={new Date(2020, 3, 19)}>
-        Olá, tudo bem?
-      </Comment>
+class App extends Component {
 
-      <Comment name="Maria" email="maria@mail.com" date={new Date(2020, 3, 21)}>
-        Olá, tudo bem sim...
-      </Comment>
-    </div>
-  );
+  state = {
+    comments:[
+      {
+        name: 'Joao',
+        email: 'joao@mail.com',
+        date: new Date(2020, 3, 19),
+        message: 'Olá, tudo bem?'
+      },
+      {
+        name: 'Maria',
+        email: 'maria@mail.com',
+        date: new Date(2020, 3, 21),
+        message: 'Olá, tudo bem sim!!'
+      },
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Meu projeto</h1>
+
+        {this.state.comments.map((comment, index) => {
+        return(
+        <Comment 
+        key={index}
+        name={comment.name}
+        email={comment.email}
+        date={comment.date}>
+          {comment.message}
+        </Comment>)
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/comment.js
+++ src/components/comment.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import '../css/comment.css';
 
-const Comment = (props) => (
+const Comment = (props) => {
+    return(
     <div className="Comment">
         <h2>{props.name}</h2>
         <p>{props.mail}</p>
         <p>{props.children}</p>
         <p>{props.date.toString()}</p>
     </div>
-);
+    )
+};
 
 export default Comment;
